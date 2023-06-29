9ff219e973e0317f95f72b25a6755a10cbb2a964
TDTA2002 <tranduytuananh203@gmail.com>
2023-06-29 17:36:52 +0700

    add file

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -3,9 +3,9 @@ import Form from 'react-bootstrap/Form';
 function SelectBasicExample() {
   return (
     <Form.Select>
-      <option value="1">All</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
+      <option value="All">All</option>
+      <option value="Incomplete">Incomplete</option>
+      <option value="Complete">Complete</option>
     </Form.Select>
   );
 }
